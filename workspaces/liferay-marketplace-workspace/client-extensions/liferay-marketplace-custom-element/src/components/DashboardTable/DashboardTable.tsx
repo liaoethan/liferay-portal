5b4d81caf0d2ce46b1fa3cd250811d9c2c05b728
Ethan Liao <marineethan@gmail.com>
2023-03-02 15:38:03 -0800

    ISSITES-6006 add loading and no apps states

--- workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/components/DashboardTable/DashboardTable.tsx
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/components/DashboardTable/DashboardTable.tsx
@@ -1,6 +1,7 @@
 import ClayTable from '@clayui/table';
 
 import swapVert from '../../assets/icons/swap-vert.svg';
+import dashboardLoadingIcon from '../../assets/images/dashboard-loading.svg';
 
 import './DashboardTable.scss';
 import {DashboardTableRow} from './DashboardTableRow';
@@ -18,50 +19,88 @@ export type AppProps = {
 };
 interface DashboardTableProps {
 	apps: AppProps[];
+	loading: boolean
 }
 
-export function DashboardTable({apps}: DashboardTableProps) {
-	return (
-		<ClayTable borderless className="dashboard-table-container">
-			<ClayTable.Head>
-				<ClayTable.Cell headingCell>
-					<div className="dashboard-table-header-name">
-						<span className="dashboard-table-header-text">
-							Name
-						</span>
-
+export function DashboardTable({apps, loading}: DashboardTableProps) {
+	if (apps.length === 0) {
+		if (loading) {
+			return (
+				<div className="dashboard-table-loading-container">
+					<div className="dashboard-table-loading-image-container">
 						<img
-							alt="Swap Vert"
-							className="dashboard-table-header-name-icon"
-							src={swapVert}
+							alt="Dashboard Loading Image"
+							className="dashboard-table-loading-image"
+							src={dashboardLoadingIcon}
 						/>
 					</div>
-				</ClayTable.Cell>
+					<div className="dashboard-table-loading-text">
+						Apps Loading . . .
+					</div>
+				</div>
+			)
+		} else {
+			return (
+				<div className="dashboard-table-loading-container">
+					<div className="dashboard-table-loading-image-container">
+						<img
+							alt="Dashboard Loading Image"
+							className="dashboard-table-loading-image"
+							src={dashboardLoadingIcon}
+						/>
+						</div>
+					<div className="dashboard-table-no-apps-text">
+						No apps yet
+					</div>
+					<div className="dashboard-table-no-apps-description">
+						Create new apps and they will show up here. Click on "<a href="/create-new-app">New App</a>" to start creating apps
+					</div>
+				</div>
+			)
+		}
+	} else {
+		return (
+			<ClayTable borderless className="dashboard-table-container">
+				<ClayTable.Head>
+					<ClayTable.Cell headingCell>
+						<div className="dashboard-table-header-name">
+							<span className="dashboard-table-header-text">
+								Name
+							</span>
 
-				<ClayTable.Cell headingCell>
-					<span className="dashboard-table-header-text">Version</span>
-				</ClayTable.Cell>
+							<img
+								alt="Swap Vert"
+								className="dashboard-table-header-name-icon"
+								src={swapVert}
+							/>
+						</div>
+					</ClayTable.Cell>
 
-				<ClayTable.Cell headingCell>
-					<span className="dashboard-table-header-text">Type</span>
-				</ClayTable.Cell>
+					<ClayTable.Cell headingCell>
+						<span className="dashboard-table-header-text">Version</span>
+					</ClayTable.Cell>
 
-				<ClayTable.Cell headingCell>
-					<span className="dashboard-table-header-text">
-						Last Updated
-					</span>
-				</ClayTable.Cell>
+					<ClayTable.Cell headingCell>
+						<span className="dashboard-table-header-text">Type</span>
+					</ClayTable.Cell>
 
-				<ClayTable.Cell headingCell>
-					<span className="dashboard-table-header-text">Status</span>
-				</ClayTable.Cell>
-			</ClayTable.Head>
+					<ClayTable.Cell headingCell>
+						<span className="dashboard-table-header-text">
+							Last Updated
+						</span>
+					</ClayTable.Cell>
 
-			<ClayTable.Body>
-				{apps.map((app) => (
-					<DashboardTableRow app={app} key={app.name} />
-				))}
-			</ClayTable.Body>
-		</ClayTable>
-	);
+					<ClayTable.Cell headingCell>
+						<span className="dashboard-table-header-text">Status</span>
+					</ClayTable.Cell>
+				</ClayTable.Head>
+
+				<ClayTable.Body>
+					{apps.map((app) => (
+						<DashboardTableRow app={app} key={app.name} />
+					))}
+				</ClayTable.Body>
+			</ClayTable>
+		);
+	}
 }
