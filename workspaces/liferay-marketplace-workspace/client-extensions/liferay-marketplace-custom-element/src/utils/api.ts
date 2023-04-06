475b09000f0316a59c5b016b9fb0e93103741634
Ethan Liao <marineethan@gmail.com>
2023-04-06 08:20:10 -0700

    ISSITES-6050 remove duplicated getAccounts method

--- workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/utils/api.ts
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/utils/api.ts
@@ -201,15 +201,6 @@ export async function deleteTrialSKU(skuTrialId: number) {
 	});
 }
 
-export async function getAccounts() {
-	const response = await fetch(
-		'/o/headless-commerce-admin-account/v1.0/accounts',
-		{headers, method: 'GET'}
-	);
-
-	return response.json();
-}
-
 export async function getAccountInfo({accountId}: {accountId: number}) {
 	const response = await fetch(
 		`/o/headless-commerce-admin-account/v1.0/accounts/${accountId}`,
