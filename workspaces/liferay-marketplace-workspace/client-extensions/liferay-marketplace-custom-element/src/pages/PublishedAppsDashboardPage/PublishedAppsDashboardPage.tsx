db046c273abc19005dc672a678655e6ea2084e17
Ethan Liao <marineethan@gmail.com>
2023-04-12 23:46:46 -0700

    ISSITES-6049 member now display account level roles instead of site level

--- workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/PublishedAppsDashboardPage/PublishedAppsDashboardPage.tsx
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/PublishedAppsDashboardPage/PublishedAppsDashboardPage.tsx
@@ -196,10 +196,12 @@ export function PublishedAppsDashboardPage() {
 		return productVersion;
 	}
 
-	function getRolesList(roles: RoleBriefProps[]) {
+	function getRolesList(accountBriefs: AccountBrief[]) {
 		const rolesList: string[] = [];
 
-		roles.forEach((role) => {
+		const accountBrief = accountBriefs.find(accountBrief => accountBrief.name === selectedAccount.name);
+
+		accountBrief?.roleBriefs.forEach((role) => {
 			rolesList.push(role.name);
 		});
 
@@ -342,7 +344,7 @@ export function PublishedAppsDashboardPage() {
 							image: member.image,
 							lastLoginDate: member.lastLoginDate,
 							name: member.name,
-							role: getRolesList(member.roleBriefs),
+							role: getRolesList(member.accountBriefs),
 							userId: member.id,
 						} as MemberProps;
 					}
